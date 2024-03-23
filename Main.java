212098e46198f012366654ea29cb2781c06cc1cf
Douglas Melo <douglasmnegri@gmail.com>
2024-03-23 15:59:34 -0300

    add console interaction to include user data

--- Main.java
+++ Main.java
@@ -1,17 +1,53 @@
-import java.time.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        User newUser01 = new User("Douglas", 28, "099.999.999-89", "random-email@gmail.com", "(0XX)9 9999-8888");
-        User newUser02 = new User("Daniel", 21, "088.888.8888-99", "daniel-mail@hotmail.com", "(0XX)9 7777-7779");
-        System.out.println("\n - User Registration Form - ");
-        newUser01.printUserInfo();
-        newUser02.printUserInfo();
+        Scanner myObj = new Scanner(System.in);
+        String name;
+        int age;
+        String cpf;
+        String email;
+        String cellPhone;
 
-        Event newEvent01 = new Event("Music Festival", "Central Park", "Music", LocalDate.of(2024, 10, 25),
-                "Experience the best bands of the world in one place!");
-        System.out.println("\n - List of Events -");
-        newEvent01.printEventInfo();
+        System.out.println("Name:");
+        while (true) {
+            name = myObj.nextLine();
+            int length = name.length();
+
+            if (length <= 1) {
+                System.out.println("Please enter a valid name.");
+            } else {
+                break;
+            }
+        }
+
+        System.out.println("Age:");
+        while (true) {
+            age = myObj.nextInt();
+            myObj.nextLine();
+
+            if(age < 0) {
+                System.out.println("Please enter a valid age.");
+            }
+            else {
+                break;
+            }
+        }
+
+        System.out.println("CPF:");
+        cpf = myObj.nextLine();
+
+        System.out.println("Email:");
+        email = myObj.nextLine();
+
+        System.out.println("Cellphone Number:");
+        cellPhone = myObj.nextLine();
+
+        // Create a User object with the input values
+        User newUser = new User(name, age, cpf, email, cellPhone);
+
+        // Call the printUserInfo() method to print the user's information
+        newUser.printUserInfo();
     }
 }
 
@@ -51,55 +87,71 @@ class User {
     }
 
     public void printUserInfo() {
-        System.out.println("\nName: " + getName());
+        System.out.println("\nUser Information:");
+        System.out.println("Name: " + getName());
         System.out.println("Age: " + getAge());
         System.out.println("CPF: " + getCpf());
         System.out.println("Email: " + getEmail());
-        System.out.println("Cell Number: " + getCellPhone());
+        System.out.println("Cellphone Number: " + getCellPhone());
     }
 }
 
-class Event {
-    private String eventName;
-    private String eventAddress;
-    private String eventCategory;
-    private LocalDate eventDate;
-    private String eventDescription;
+// class Event {
+// private String eventName;
+// private String eventAddress;
+// private String eventCategory;
+// private LocalDate eventDate;
+// private String eventDescription;
 
-    public Event(String eventName, String eventAddress, String eventCategory, LocalDate eventDate,
-            String eventDescription) {
-        this.eventName = eventName;
-        this.eventAddress = eventAddress;
-        this.eventCategory = eventCategory;
-        this.eventDate = eventDate;
-        this.eventDescription = eventDescription;
-    }
+// public Event(String eventName, String eventAddress, String eventCategory,
+// LocalDate eventDate,
+// String eventDescription) {
+// this.eventName = eventName;
+// this.eventAddress = eventAddress;
+// this.eventCategory = eventCategory;
+// this.eventDate = eventDate;
+// this.eventDescription = eventDescription;
+// }
 
-    public String getEventName() {
-        return eventName;
-    }
+// public String getEventName() {
+// return eventName;
+// }
 
-    public String getEventAddress() {
-        return eventAddress;
-    }
+// public String getEventAddress() {
+// return eventAddress;
+// }
 
-    public String getEventCategory() {
-        return eventCategory;
-    }
+// public String getEventCategory() {
+// return eventCategory;
+// }
 
-    public LocalDate getEventDate() {
-        return eventDate;
-    }
+// public LocalDate getEventDate() {
+// return eventDate;
+// }
 
-    public String getEventDescription() {
-        return eventDescription;
-    }
+// public String getEventDescription() {
+// return eventDescription;
+// }
 
-    public void printEventInfo() {
-        System.out.println("Name: " + getEventName());
-        System.out.println("Address: " + getEventAddress());
-        System.out.println("Category: " + getEventCategory());
-        System.out.println("Date: " + getEventDate());
-        System.out.println("Description: " + getEventDescription());
-    }
-}
+// public void printEventInfo() {
+// System.out.println("Name: " + getEventName());
+// System.out.println("Address: " + getEventAddress());
+// System.out.println("Category: " + getEventCategory());
+// System.out.println("Date: " + getEventDate());
+// System.out.println("Description: " + getEventDescription());
+// }
+// }
+
+// // User newUser01 = new User("Douglas", 28, "099.999.999-89",
+// // "random-email@gmail.com", "(0XX)9 9999-8888");
+// // User newUser02 = new User("Daniel", 21, "088.888.8888-99",
+// // "daniel-mail@hotmail.com", "(0XX)9 7777-7779");
+// // System.out.println("\n - User Registration Form - ");
+// // newUser01.printUserInfo();
+// // newUser02.printUserInfo();
+
+// // Event newEvent01 = new Event("Music Festival", "Central Park", "Music",
+// // LocalDate.of(2024, 10, 25),
+// // "Experience the best bands of the world in one place!");
+// // System.out.println("\n - List of Events -");
+// // newEvent01.printEventInfo();
